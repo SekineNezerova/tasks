f5262dacac623dbe2541966ddd3f89f9fda71017
Sekine Nezerova <7b4gz6k@code.edu.az>
2022-03-15 08:42:32 +0400

    added tasks

---
+++ Prog.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //1.Verilmiş ədədin sadə ədəd olub olmamasını print edən console app yazın
+
+            //Console.WriteLine("ededi daxil et:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 2; i < n; i++)
+            //{
+            //    if (n % i == 0)
+            //    {
+            //        Console.WriteLine("eded murekkebdir");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("eded sadedir");
+            //        break;
+            //    }
+
+
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region task2
+            //2.Verilmiş ədədin neçə mərtəbəli olduğunu print edən console app yazın.
+
+            //Console.WriteLine("ededi daxil et:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int m, say = 0;
+            //if (n==0)
+            //{
+            //    Console.WriteLine(1);
+            //    Console.ReadKey();
+            //    return;
+            //}
+            //else
+            //{
+            //    while (n!=0)
+            //    {
+            //        m = n % 10;
+            //        say = say + 1;
+            //        n = n / 10;
+            //    }
+            //}
+
+            //Console.WriteLine(say);
+            //Console.ReadKey();
+            #endregion
+
+            #region task3
+            //3.Verilmiş ədədin palindrom olub olmadığını print edən  console app yazın.
+
+            //Console.WriteLine("ededi daxil et:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int qaliq, ters = 0;
+            //int m = n;
+            //if (n < 9 && n >= 0)
+            //{
+            //    Console.WriteLine("palindromdur");
+            //    Console.ReadKey();
+            //}
+            //else
+            //{
+
+            //    while (m > 0)
+            //    {
+            //        qaliq = m % 10;
+            //        ters = ters * 10 + qaliq;
+            //        m = m / 10;
+            //    }
+            //    if (n == ters)
+            //    {
+            //        Console.WriteLine("Eded palindromdur");
+            //        Console.ReadKey();
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded palindrom deyil");
+            //        Console.ReadKey();
+            //    }
+
+            //}
+            #endregion
+
+        }
+    }
+}
